9dc4d63a3868df0db9d7a8b81302cfe649466dc1
Krzysztofkoj <krzysiekpoloska@gmail.com>
2022-09-15 12:43:52 +0200

    Losowanie do tablicy

--- src/main.java
+++ src/main.java
@@ -1,3 +1,13 @@
 public class main {
-
+    public static void main(String[] args) {
+        System.out.println("Losowanie 6 liczb");
+        int [] wylosowane =new int[6];
+        for(int i=0; i<wylosowane.length;i++){
+            wylosowane[i] =(int)(Math.random()*100 +1);
+        }
+        for(int wylosowaneWartosc :wylosowane){
+            System.out.println(wylosowaneWartosc+"");
+        }
+    }
 }
+
