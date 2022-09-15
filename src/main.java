0771fca19b87689f5fc2dbb50e48afa4ba57b743
Krzysztofkoj <krzysiekpoloska@gmail.com>
2022-09-15 14:10:58 +0200

    elementy nie mog si powtarzac

--- src/main.java
+++ src/main.java
@@ -1,32 +1,30 @@
-import org.w3c.dom.ls.LSOutput;
 
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+
+import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-       Set<Integer> wylosowane = new HashSet<>();
-       for(int i = 0 ; i<6;i++){
-           wylosowane.add((int)(Math.random()*10));
-       }
-        System.out.println("wylosowane:"+wylosowane);
+        Set<Integer> wylosowane = new HashSet<>();
+        for (int i = 0; i < 6; i++) {
+            wylosowane.add((int) (Math.random() * 10));
+        }
+        System.out.println("wylosowane:" + wylosowane);
 
 
         System.out.println("Podaj 6 liczb");
-        for(int i=0;i<6;i++){
-            System.out.println("Liczba "+i);
+        List<Integer> wpisane = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Liczba " + i);
+            Scanner klawiatura = new Scanner(System.in);
             wpisane.add(klawiatura.nextInt());
-            int wczytanaWartosc =klawiatura.nextInt();
-            while(wpisane.contains(wczytanaWartosc)){
+            int wczytanaWartosc = klawiatura.nextInt();
+            while (wpisane.contains(wczytanaWartosc)) {
                 System.out.println("Taka liczba już została podana podaj inną");
                 wczytanaWartosc = klawiatura.nextInt();
             }
             wpisane.add(wczytanaWartosc);
         }
-        System.out.println("Wpisane "+wpisane);
-    }
-
+        System.out.println("Wpisane " + wpisane);
     }
 }
 
