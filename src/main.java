26dd282602290f8f0ab351dbd021500de7850bd4
Krzysztofkoj <krzysiekpoloska@gmail.com>
2022-09-15 13:42:52 +0200

    wpisywanie

--- src/main.java
+++ src/main.java
@@ -1,4 +1,7 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class main {
@@ -8,6 +11,22 @@ public class main {
            wylosowane.add((int)(Math.random()*10));
        }
         System.out.println("wylosowane:"+wylosowane);
+
+
+        System.out.println("Podaj 6 liczb");
+        for(int i=0;i<6;i++){
+            System.out.println("Liczba "+i);
+            wpisane.add(klawiatura.nextInt());
+            int wczytanaWartosc =klawiatura.nextInt();
+            while(wpisane.contains(wczytanaWartosc)){
+                System.out.println("Taka liczba już została podana podaj inną");
+                wczytanaWartosc = klawiatura.nextInt();
+            }
+            wpisane.add(wczytanaWartosc);
+        }
+        System.out.println("Wpisane "+wpisane);
+    }
+
     }
 }
 
